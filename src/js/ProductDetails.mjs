1babc92ef97fd0231953f18261e2930ea41b09cc
olowo-elisha-ibukun <elishaibukunolowo@gmail.com>
2026-05-14 10:08:35 +0100

    olowo

--- src/js/ProductDetails.mjs
+++ src/js/ProductDetails.mjs
@@ -1,10 +1,5 @@
 import { setLocalStorage } from './utils.mjs';
 
-const tentImages = Object.fromEntries(
-  Object.entries(import.meta.glob('../images/tents/*', { eager: true, query: '?url' }))
-    .map(([path, url]) => [path.split('/').pop(), url]),
-);
-
 export default class ProductDetails {
   constructor(productId, dataSource) {
     this.productId = productId;
@@ -26,18 +21,13 @@ export default class ProductDetails {
       .addEventListener('click', this.addToCart.bind(this));
   }
 
-  getProductImageUrl() {
-    const imageName = this.product.Image?.split('/').pop();
-    return imageName ? tentImages[imageName] || this.product.Image : this.product.Image;
-  }
-
   renderProductDetails() {
     const main = document.querySelector('main');
     main.innerHTML = `
       <div class="product-detail">
         <h2>${this.product.Brand.Name}</h2>
         <h1>${this.product.Name}</h1>
-        <img class="divider" src="${this.getProductImageUrl()}" alt="${this.product.Name}" />
+        <img class="divider" src="${this.product.Image}" alt="${this.product.Name}" />
         <p class="product-card__price">$${this.product.FinalPrice}</p>
         <p class="product-color">Color: ${this.product.Colors[0].ColorName}</p>
         <p class="product-description">${this.product.DescriptionHtmlSimple}</p>
