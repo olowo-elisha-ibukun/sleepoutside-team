c2b8d7ee298aec0f073ebdd1f74ada36b7e2526b
olowo-elisha-ibukun <elishaibukunolowo@gmail.com>
2026-05-15 21:51:02 +0100

    Olowo

--- src/js/ProductDetails.mjs
+++ src/js/ProductDetails.mjs
@@ -27,7 +27,7 @@ export default class ProductDetails {
       <div class="product-detail">
         <h2>${this.product.Brand.Name}</h2>
         <h1>${this.product.Name}</h1>
-        <img class="divider" src="${this.product.Image}" alt="${this.product.Name}" />
+        <img class="divider" src="${this.product.Image}" alt="Image of ${this.product.Name}" />
         <p class="product-card__price">$${this.product.FinalPrice}</p>
         <p class="product-color">Color: ${this.product.Colors[0].ColorName}</p>
         <p class="product-description">${this.product.DescriptionHtmlSimple}</p>
